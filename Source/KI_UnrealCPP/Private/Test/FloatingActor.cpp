059cf4737b970960250e4498172e51e1ef801d7c
go2665 <go2665@outlook.com>
2025-11-07 16:53:17 +0900

    실습 풀이 - Floating Actor

--- Source/KI_UnrealCPP/Private/Test/FloatingActor.cpp
+++ Source/KI_UnrealCPP/Private/Test/FloatingActor.cpp
@@ -22,15 +22,35 @@ void AFloatingActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	//BodyMesh->SetRelativeLocation(FVector(0, 0, 100));
-	//BodyMesh->AddRelativeLocation
+}
+
+void AFloatingActor::OnFloatingMeshUpdate(float DeltaTime)
+{
+	//BodyMesh->AddRelativeLocation(DeltaTime * Speed * FVector::UpVector);
+
+	//if (BodyMesh->GetRelativeLocation().Z > MoveHeight
+	//	|| BodyMesh->GetRelativeLocation().Z < 0)
+	//{
+	//	Speed *= -1.0f;
+	//}
+
+	ElapsedTime += DeltaTime;	// 시간 누적시키기
+	//UE_LOG(LogTemp, Log, TEXT("Elapsed Time : %.2f"), ElapsedTime);
+
+	// Cos 함수 이용해서 위치기준 업데이트
+	float cosValue = FMath::Cos(ElapsedTime);	// 1 -> -1 -> 1
+	cosValue += 1;				// 2 -> 0 -> 2
+	cosValue *= 0.5f;			// 1 -> 0 -> 1
+	cosValue = 1 - cosValue;	// 0 -> 1 -> 0
+
+	BodyMesh->SetRelativeLocation(FVector(0, 0, cosValue * MoveHeight));	// 위치 적용
+	BodyMesh->AddRelativeRotation(FRotator(0, SpinSpeed * DeltaTime, 0));	// 회전 적용
 }
 
 // Called every frame
 void AFloatingActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	BodyMesh->AddRelativeLocation(DeltaTime * Speed * FVector::UpVector);
+	OnFloatingMeshUpdate(DeltaTime);
 }
 
