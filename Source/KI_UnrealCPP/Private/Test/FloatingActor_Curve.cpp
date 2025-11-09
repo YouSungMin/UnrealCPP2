5d97b0a6cb14403dba5cbeeaf978975645710570
go2665 <go2665@outlook.com>
2025-11-10 08:59:08 +0900

    로그 삭제

--- Source/KI_UnrealCPP/Private/Test/FloatingActor_Curve.cpp
+++ Source/KI_UnrealCPP/Private/Test/FloatingActor_Curve.cpp
@@ -13,8 +13,8 @@ void AFloatingActor_Curve::OnFloatingMeshUpdate(float DeltaTime)
 		float curveInput = FMath::Fmod(ElapsedTime / Duration, 1.0f);	// Duration을 이용해 한주기 시간 결정, 0.0~1.0범위의 숫자로 반복
 		float curveValue = HeightCurve->GetFloatValue(curveInput);		// 0~1~0
 
-		UE_LOG(LogTemp, Log, TEXT("Input : %.2f"), curveInput);
-		UE_LOG(LogTemp, Log, TEXT("Height : %.2f"), curveValue);
+		//UE_LOG(LogTemp, Log, TEXT("Input : %.2f"), curveInput);
+		//UE_LOG(LogTemp, Log, TEXT("Height : %.2f"), curveValue);
 		BodyMesh->SetRelativeLocation(FVector(0, 0, curveValue * MoveHeight));
 		BodyMesh->AddRelativeRotation(FRotator(0, SpinSpeed * DeltaTime, 0));
 	}
