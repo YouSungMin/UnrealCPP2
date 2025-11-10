271d0696fa9131aeab0e2b2b8e5740679c9b736e
go2665 <go2665@outlook.com>
2025-11-10 13:39:55 +0900

    실습풀이 - 캐릭터 이동, 컴포넌트 추가

--- Source/KI_UnrealCPP/Public/Player/ActionCharacter.h
+++ Source/KI_UnrealCPP/Public/Player/ActionCharacter.h
@@ -8,6 +8,7 @@
 #include "ActionCharacter.generated.h"
 
 class UInputAction;
+//class USpringArmComponent;
 
 UCLASS()
 class KI_UNREALCPP_API AActionCharacter : public ACharacter
@@ -31,6 +32,13 @@ public:
 
 protected:
 	void OnMoveInput(const FInputActionValue& InValue);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player|Camera")
+	TObjectPtr<class USpringArmComponent> SpringArm = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player|Camera")
+	TObjectPtr<class UCameraComponent> PlayerCamera = nullptr;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	TObjectPtr<UInputAction> IA_Move;
