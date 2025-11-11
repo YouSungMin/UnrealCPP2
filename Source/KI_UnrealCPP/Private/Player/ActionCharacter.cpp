9f561e06eb41575cbecd9e3ad392be4bd91f9808
go2665 <go2665@outlook.com>
2025-11-11 13:57:26 +0900

    구르기를 할 때 마지막 입력방향으로 회전시킬 때 버그 수정

--- Source/KI_UnrealCPP/Private/Player/ActionCharacter.cpp
+++ Source/KI_UnrealCPP/Private/Player/ActionCharacter.cpp
@@ -84,7 +84,10 @@ void AActionCharacter::OnRollInput(const FInputActionValue& InValue)
 	{
 		if (!AnimInstance->IsAnyMontagePlaying())
 		{
-			//SetActorRotation(GetLastMovementInputVector().Rotation());	// 마지막 입력 방향으로 회전 시키기
+			//if (!GetLastMovementInputVector().IsNearlyZero())	// 입력을 하는 중에만 즉시 회전
+			//{
+			//	SetActorRotation(GetLastMovementInputVector().Rotation());	// 마지막 입력 방향으로 즉시 회전 시키기
+			//}
 			PlayAnimMontage(RollMontage);
 		}
 	}
