39d53dce6c6b870e64c80dfecd4354df7a4a9af3
go2665 <go2665@outlook.com>
2025-11-17 15:26:13 +0900

    픽업을 두번 먹는 버그 수정, 타임라인 시간 설정

--- Source/KI_UnrealCPP/Private/Item/Pickup.cpp
+++ Source/KI_UnrealCPP/Private/Item/Pickup.cpp
@@ -3,6 +3,7 @@
 
 #include "Item/Pickup.h"
 #include "Components/SphereComponent.h"
+#include "Components/TimelineComponent.h"
 #include "NiagaraComponent.h"
 #include "Player/InventoryOwner.h"
 
@@ -33,6 +34,8 @@ APickup::APickup()
 	Effect = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Effect"));
 	Effect->SetupAttachment(BaseRoot);
 
+	PickupTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("PickupTimeline"));
+
 }
 
 // Called when the game starts or when spawned
@@ -44,6 +47,24 @@ void APickup::BeginPlay()
 	{
 		PickupOverlap->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnPickupBeginOverlap);
 	}
+
+	if (PickupTimeline)
+	{
+		if (ScaleCurve)
+		{
+			FOnTimelineFloat ScaleUpdateDelegate;
+			ScaleUpdateDelegate.BindUFunction(this, FName("OnScaleUpdate"));
+			PickupTimeline->AddInterpFloat(ScaleCurve, ScaleUpdateDelegate);
+
+			FOnTimelineEvent ScaleFinishDelegate;
+			ScaleFinishDelegate.BindUFunction(this, FName("OnScaleFinish"));
+			PickupTimeline->SetTimelineFinishedFunc(ScaleFinishDelegate);
+		}
+
+		PickupTimeline->SetPlayRate(1/Duration);
+	}
+
+	bPickuped = false;
 }
 
 // Called every frame
@@ -56,16 +77,32 @@ void APickup::Tick(float DeltaTime)
 
 void APickup::OnPickup_Implementation(AActor* Target)
 {
-	//UE_LOG(LogTemp, Log, TEXT("OnPickup_Implementation 실행"));
-	// 자신을 먹은 대상에게 자기가 가지고 있는 무기를 알려줘야 함
-	if (Target && Target->Implements<UInventoryOwner>())
+	if (!bPickuped)
 	{
-		IInventoryOwner::Execute_AddItem(Target, PickupItem);
+		UE_LOG(LogTemp, Log, TEXT("OnPickup_Implementation 실행"));
+		bPickuped = true;
+		PickupOwner = Target;
+		PickupTimeline->PlayFromStart();	// 타임라인 시작
 	}
 }
 
 void APickup::OnPickupBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	//UE_LOG(LogTemp, Log, TEXT("Pickup Overlap"));
+}
+
+void APickup::OnScaleUpdate(float Value)
+{
+	FVector NewScale = FVector::One() * Value;
+	SetActorScale3D(NewScale);
+}
+
+void APickup::OnScaleFinish()
+{
+	// 자신을 먹은 대상에게 자기가 가지고 있는 무기를 알려줘야 함
+	if (PickupOwner.IsValid() && PickupOwner->Implements<UInventoryOwner>())
+	{
+		IInventoryOwner::Execute_AddItem(PickupOwner.Get(), PickupItem);
+	}
 }
 
--- Source/KI_UnrealCPP/Public/Item/Pickup.h
+++ Source/KI_UnrealCPP/Public/Item/Pickup.h
@@ -38,6 +38,12 @@ private:
 		bool bFromSweep, 
 		const FHitResult& SweepResult);
 
+	UFUNCTION()
+	void OnScaleUpdate(float Value);
+
+	UFUNCTION()
+	void OnScaleFinish();
+
 protected:
 	// 물리 적용용 루트
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
@@ -53,7 +59,11 @@ protected:
 
 	// 아이템 이펙트
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	TObjectPtr<class UNiagaraComponent> Effect;
+	TObjectPtr<class UNiagaraComponent> Effect = nullptr;
+
+	// 타임라인 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<class UTimelineComponent> PickupTimeline = nullptr;
 
 	// 이 픽업을 먹었을 때 얻을 아이템
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pickup")
@@ -62,4 +72,19 @@ protected:
 	// 아이템 회전 속도
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
 	float RotateSpeed = 180.0f;
+
+	// 픽업 획득 효과용 스케일 커브
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pickup")
+	TObjectPtr<UCurveFloat> ScaleCurve = nullptr;
+
+	// 아이템 획득에 걸리는 시간(초)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
+	float Duration = 0.5f;
+
+private:
+	// 이 픽업 아이템을 먹은 액터
+	TWeakObjectPtr<AActor> PickupOwner = nullptr;
+
+	// 획득 되었는지 여부(true면 획득 처리 중)
+	bool bPickuped = false;
 };
